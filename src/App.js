c7730c18a0dc8ef20f881bcb9250de0996a6cfbb
renansistemas <naner.silva.freitas@gmail.com>
2024-11-17 17:40:50 -0300

    init commit desafio linguagem favorita

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleVote = (option) => {
+    alert(`Você escolheu: ${option}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Qual sua linguagem de programação favorita?</h1>
+      <div style={{ marginTop: "20px" }}>
+        {/* Botões das opções */}
+        <button
+          onClick={() => handleVote("JavaScript")}
+          style={{
+            padding: "10px 20px",
+            margin: "5px",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          JavaScript
+        </button>
+        <button
+          onClick={() => handleVote("Python")}
+          style={{
+            padding: "10px 20px",
+            margin: "5px",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+        >
+          Python
+        </button>
+        <button
+          onClick={() => handleVote("Java")}
+          style={{
+            padding: "10px 20px",
+            margin: "5px",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+        >
+          Java
+        </button>
+      </div>
     </div>
   );
 }
